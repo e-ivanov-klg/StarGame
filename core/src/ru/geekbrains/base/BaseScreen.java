845be69aaf2453f1adc30463de8ba14d6f77c90b
Evgeny Ivanov <ei.kaluga@gmail.com>
2020-04-29 22:50:26 +0300

    before homework2

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -1,17 +1,19 @@
 package ru.geekbrains.base;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BaseScreen implements Screen {
+public class BaseScreen implements Screen, InputProcessor {
     protected SpriteBatch batch;
-
 
     @Override
     public void show() {
         // Инициализация всего, что происходит. аналог метода create
         System.out.println("show");
+        Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
     }
 
@@ -48,5 +50,55 @@ public class BaseScreen implements Screen {
     public void dispose() {
         System.out.println("despose");
         batch.dispose();
+    }
+    // -------------------------------------------------------------------
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown, keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("keyUp, keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("keyTyped, char = " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        //координаты, номер пальца, номер кнопки
+        System.out.println("touchDown = " + screenX + ", " + screenY + "; pointer = " + pointer + "; button = " + button);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUp = " + screenX + ", " + screenY + "; pointer = " + pointer + "; button = " + button);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        // После перетаскивания
+        System.out.println("touchDragged = " + screenX + ", " + screenY + "; pointer = " + pointer);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        // движение колесика amount 1 или -1 в зависимости отнаправоения
+        System.out.println("scrolled = " + amount);
+        return false;
     }
 }
