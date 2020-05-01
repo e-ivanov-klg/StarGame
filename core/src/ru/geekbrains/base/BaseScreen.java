c54c1e713861e3e4328d0ed2cb23a1e33673361a
Evgeny Ivanov <ei.kaluga@gmail.com>
2020-05-01 21:10:08 +0300

    Homework lesson 02

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -69,7 +69,6 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        //System.out.println("touchUp = " + screenX + ", " + screenY + "; pointer = " + pointer + "; button = " + button);
         return false;
     }
 
