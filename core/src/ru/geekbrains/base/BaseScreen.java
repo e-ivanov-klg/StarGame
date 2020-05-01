d1977ed0a6c3796a10e9457f721f71597cba76aa
Evgeny Ivanov <ei.kaluga@gmail.com>
2020-05-01 21:04:25 +0300

    Homework lesson 02

--- core/src/ru/geekbrains/base/BaseScreen.java
+++ core/src/ru/geekbrains/base/BaseScreen.java
@@ -12,7 +12,6 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public void show() {
         // Инициализация всего, что происходит. аналог метода create
-        //System.out.println("show");
         Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
     }
@@ -25,55 +24,46 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public void resize(int width, int height) {
         // Когда поменяли размер экрана
-        //System.out.println("width = " + width + "; height = " + height);
     }
 
     @Override
     public void pause() {
         // когда свернули приложение
-        //System.out.println("pause");
     }
 
     @Override
     public void resume() {
         // Когда развернули приложение
-        //System.out.println("resume");
     }
 
     @Override
     public void hide() {
-        //System.out.println("hide");
         dispose();
     }
 
     @Override
     public void dispose() {
-        //System.out.println("despose");
         batch.dispose();
     }
     // -------------------------------------------------------------------
     @Override
     public boolean keyDown(int keycode) {
-        //System.out.println("keyDown, keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyUp(int keycode) {
-        //System.out.println("keyUp, keycode = " + keycode);
         return false;
     }
 
     @Override
     public boolean keyTyped(char character) {
-        //System.out.println("keyTyped, char = " + character);
         return false;
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         //координаты, номер пальца, номер кнопки
-        //System.out.println("touchDown = " + screenX + ", " + screenY + "; pointer = " + pointer + "; button = " + button);
         return false;
     }
 
@@ -86,7 +76,6 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         // После перетаскивания
-        //System.out.println("touchDragged = " + screenX + ", " + screenY + "; pointer = " + pointer);
         return false;
     }
 
@@ -98,7 +87,6 @@ public class BaseScreen implements Screen, InputProcessor {
     @Override
     public boolean scrolled(int amount) {
         // движение колесика amount 1 или -1 в зависимости отнаправоения
-        //System.out.println("scrolled = " + amount);
         return false;
     }
 }
